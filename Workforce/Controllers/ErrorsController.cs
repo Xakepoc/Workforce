fc5078c09d974a9f8e54d149e3c17498fb64d2d1
Mikhail Pegeev <mikhail.pegeev@gmail.com>
2013-09-30 15:40:02 +0400

    Added NLog

---
+++ Workforce/Controllers/ErrorsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Workforce.Controllers
+{
+	public class ErrorsController : Controller
+	{
+		public ActionResult General()
+		{
+			var exc = RouteData.Values["Exception"] as System.Exception;
+			ViewBag.WindowTite = "Error";
+			return View("General");
+		}
+
+		public ActionResult PageNotFound()
+		{
+			return View("PageNotFound");
+		}
+
+		public ActionResult InternalServerError()
+		{
+			ViewBag.WindowTite = "Internal error";
+			return View("General");
+		}
+	}
+}
--- Workforce/Global.asax.cs
+++ Workforce/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
+using Workforce.Controllers;
 
 namespace Workforce
 {
@@ -14,6 +16,8 @@ namespace Workforce
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -21,6 +25,55 @@ namespace Workforce
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            var exc = Server.GetLastError().GetBaseException();
+
+            Response.Clear();
+
+            var httpExc = exc as HttpException;
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Errors");
+
+            if (httpExc == null)
+            {
+                _logger.Error(exc);
+                routeData.Values.Add("action", "General");
+            }
+            else //It's an Http Exception, Let's handle it.
+            {
+                switch (httpExc.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values.Add("action", "PageNotFound");
+                        Response.StatusCode = 404;
+                        break;
+                    case 500:
+                        _logger.Error(exc);
+                        routeData.Values.Add("action", "InternalServerError");
+                        break;
+                    default:
+                        _logger.Error(exc);
+                        routeData.Values.Add("action", "General");
+                        break;
+                }
+            }
+
+            // Pass exception details to the target error View.
+            routeData.Values.Add("Exception", exc);
+
+            // Clear the error on server.
+            Server.ClearError();
+
+            // Avoid IIS7 getting in the middle
+            Response.TrySkipIisCustomErrors = true;
+
+            // Call target Controller and pass the routeData.
+            IController errorController = new ErrorsController();
+            errorController.Execute(new RequestContext(
+                        new HttpContextWrapper(Context), routeData));
         }
     }
 }
